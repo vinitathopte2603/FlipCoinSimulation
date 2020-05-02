620b9a6fa4bd8ff2e6e1e68b1a9091f5a4f3fe00
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-02 22:46:14 +0530

    [Vinita]ADDED:FlipCoinMultipleTimesToDetermineSingletCombination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,10 +4,20 @@ var="Welcome to flip coin simulator program"
 echo "$var"
 
 heads=1
+headsCount=0
+numOfFlips=10
+declare -A resultSinglet
+for (( i=1;i<=$numOfFlips;i++ ))
+do
 flip=$((RANDOM%2))
 	if [ $flip -eq $heads ]
 	then
-		echo "Heads"
+		((headsCount++))
 	else
-		echo "Tails"
+		((tailsCount++))
 	fi
+done
+resultSinglet["Heads"]=$((($headsCount*100)/$numOfFlips))
+resultSinglet["Tails"]=$((($tailsCount*100)/$numOfFlips))
+echo ${!resultSinglet[@]}
+echo ${resultSinglet[@]}
