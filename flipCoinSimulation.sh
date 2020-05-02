a225ce45809491fb40fa39985402d1fc3df00000
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-02 23:02:54 +0530

    [Vinita]ADDED:FlipCoinMultipleTimesToDetermineDoubletCombination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -21,3 +21,32 @@ resultSinglet["Heads"]=$((($headsCount*100)/$numOfFlips))
 resultSinglet["Tails"]=$((($tailsCount*100)/$numOfFlips))
 echo ${!resultSinglet[@]}
 echo ${resultSinglet[@]}
+
+declare -A resultDoublet
+resultDoublet+=( ["HH"]=0 ["TT"]=0 ["HT"]=0 ["TH"]=0 )
+
+for (( i=1;i<=$numOfFlips;i++ ))
+do
+	flip=$(( RANDOM%4 ))
+	case $flip in
+		0)
+			((resultDoublet["HH"]++))
+		;;
+		1)
+			((resultDoublet["TT"]++))
+		;;
+		2)
+			((resultDoublet["HT"]++))
+		;;
+		3)
+			((resultDoublet["TH"]++))
+		;;
+		*)
+		;;
+	esac
+done
+for key in ${!resultDoublet[@]}
+do
+resultDoublet[$key]=$(((${resultDoublet[$key]}*100)/$numOfFlips))
+printf "$key : ${resultDoublet[$key]}"
+done
